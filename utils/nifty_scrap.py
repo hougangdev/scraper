fe7aaeff77c3756a012c89d9a7eb35020b9c4f76
hougangdev <devchain7890@gmail.com>
2024-05-04 16:14:52 +0800

    removed headless, website cannot render

--- utils/nifty_scrap.py
+++ utils/nifty_scrap.py
@@ -16,7 +16,6 @@ load_dotenv()
 
 def initialize_driver():
     options = Options()
-    # options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
